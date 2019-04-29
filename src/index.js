8ed6463a2e30f29ae30ef78481b6754f5da58eb4
Madeline Emde <madeline.emde@gmail.com>
2019-04-29 14:55:26 -0400

    working minus bonus styling

--- src/index.js
+++ src/index.js
@@ -11,12 +11,5 @@ const map = new mapboxgl.Map({
   style: "mapbox://styles/mapbox/streets-v10"
 });
 
-// const newDom = document.createElement("div");
-// newDom.style.width = "32px";
-// newDom.style.height = "39px";
-// newDom.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
-// // new mapboxgl.Marker(newDom).setLngLat([-74.009151, 40.705086]).addTo(map);
-const newMarker = marker("activity", [-74.009151, 40.705086]);
-
+const newMarker = marker("activities", [-74.009151, 40.705086]);
 newMarker.addTo(map);
-console.log(newMarker);
--- src/marker.js
+++ src/marker.js
@@ -1,26 +1,19 @@
 const mapboxgl = require("mapbox-gl");
 
-let diffActs = {
-  activities: "http://i.imgur.com/WbMOfMl.png",
+const iconURLs = {
   hotels: "http://i.imgur.com/D9574Cu.png",
-  restaurants: "http://i.imgur.com/cqR6pUI.png"
+  restaurants: "http://i.imgur.com/cqR6pUI.png",
+  activities: "http://i.imgur.com/WbMOfMl.png"
 };
 
-function marker(activity, latLong) {
-  // mapboxgl.Marker(newDom).setLngLat([-74.009151, 40.705086]).addTo(map)
-  const anotherDom = document.createElement("div");
-  anotherDom.style.width = "32px";
-  anotherDom.style.height = "39px";
-  anotherDom.style.backgroundImage = diffActs.activity;
+function marker(activity, coords) {
+  const markerDom = document.createElement("div");
+  markerDom.style.width = "32px";
+  markerDom.style.height = "39px";
+  markerDom.style.backgroundImage = `url(${iconURLs[activity]})`;
+  console.log("markerDom", markerDom, "ICON URLS:", iconURLs[activity]);
 
-  console.log(anotherDom);
-
-  const newMarker = mapboxgl
-    .Marker(anotherDom)
-    .setLngLat(latLong)
-    .addTo(map);
-
-  console.log(newMarker);
+  const newMarker = new mapboxgl.Marker(markerDom).setLngLat(coords);
 
   return newMarker;
 }
